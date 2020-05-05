615398935d13df9bba2afbad910140fdbdd43201
Duy Nguyen <duy.nguyen1@intranet-trg.com>
2020-05-05 20:56:36 +0700

    Add all files

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    public float flyPower;
+    GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = gameObject;
+        flyPower = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, flyPower));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("End Game");
+    }
+}
---
+++ Assets/Scripts/PipeGreenMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeGreenMove : MonoBehaviour
+{
+    public float moveSpeed;
+    public float minY;
+    public float maxY;
+    public float oldPosition;
+
+    private GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = gameObject;
+        moveSpeed = 3;
+        oldPosition = 20;
+        minY = -1;
+        maxY = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obj.transform.Translate(new Vector3(-1 * Time.deltaTime * moveSpeed, 0, 0));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("ResetPipeGreen"))
+        {
+            float random = Random.Range(minY, maxY);
+            obj.transform.position = new Vector3(oldPosition, random, 0);
+        }
+    }
+}
